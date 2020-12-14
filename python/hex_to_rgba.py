1cd4fd45dc677c65adb1243df86d4bbff17eada7
Eder Oliveira Lima <64004309+Nxrth-x@users.noreply.github.com>
2020-12-14 10:45:10 -0300

    Update hex_to_rgba.py

--- python/hex_to_rgba.py
+++ python/hex_to_rgba.py
@@ -1,7 +1,7 @@
-def hex_to_rgba(hex_value):
-	hex_value = hex_value.replace("#", "")
+def hex_to_rgba(hex_value: str) -> str:
+	hex_value: str = hex_value.replace("#", "")
 	assert len(hex_value) == 6
-	values = [hex_value[i:i+2] for i in range(0, 6, 2)]
+	values: list = [hex_value[i:i+2] for i in range(0, 6, 2)]
 	for index, value in enumerate(values):
 		values[index] = int(value, 16)
 	return f"rgba({values[0]}, {values[1]}, {values[2]}, 1)"
